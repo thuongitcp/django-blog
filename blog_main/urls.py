42dc9d0322f4a5157a46bc03fdb1d493511816da
Alan Nguyen <thuongitcp@gmail.com>
2024-03-26 16:34:26 +0700

    posts by category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from . import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    path('category/', include('blogs.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from .models import Blog, Category
+
+def posts_by_category(request, category_id):
+    # Fetch the posts that belong to the category with the id category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    # Use try/except when we want to do some custom action if the category does not exist
+    try:
+        category = Category.objects.get(id=category_id)
+    except:
+        # Redirect the user to homepage or render to 404 page
+        return render(request,'404.html')
+    # Use get_object_or_404 when you want to show 404 error page if the category does not exist
+    # category = get_object_or_404(Category, pk=category_id)
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
