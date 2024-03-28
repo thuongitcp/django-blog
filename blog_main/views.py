cacaf17b762e7d6ea57da654b7124e38b0290183
Alan Nguyen <thuongitcp@gmail.com>
2024-03-28 20:04:23 +0700

    registration and login functional

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from assignments.models import About
+from blog_main.forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 from blogs.models import Blog, Category
 
 
@@ -19,3 +22,46 @@ def home(request):
         'about': about,
     }
     return render(request, 'home.html', context)
+
+def register(request):
+
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else: 
+            print(form.errors)
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    # messages.success(request, 'You are logged out')
+    return redirect('login')
